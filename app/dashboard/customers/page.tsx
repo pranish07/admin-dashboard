f875a3b839d99048f271582902a47b5f463eddbd
pranish07 <pranishshrestha181@gmail.com>
2024-04-10 08:51:38 +0545

    nextjs added

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function Page(){
+    return(
+        <div>
+            This is a customers page
+        </div>
+    )
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function dashboard(){
+    return(
+        <div>
+            <p>This is a dashboard</p>
+        </div>
+    )
+}
