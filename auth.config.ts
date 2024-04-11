3637a9635768a472e4c4f4c6c7b34d158f7e49d5
pranish07 <pranishshrestha181@gmail.com>
2024-04-11 08:10:24 +0545

    test1- solving prod. problem

--- auth.config.ts
+++ auth.config.ts
@@ -17,6 +17,7 @@ export const authConfig = {
       return true;
     },
   },
+  secret: process.env.AUTH_SECRET,
   providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
 
