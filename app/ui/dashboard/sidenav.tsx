c4d7d9481edc07a9090a2401131add4d0a98af8e
pranish07 <pranishshrestha181@gmail.com>
2024-04-11 08:18:49 +0545

    fixing prod. problem

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -3,7 +3,6 @@ import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import ThemeSwitch from '@/app/components/ThemeSwitch';
-import { signOut } from '@/auth';
 
 export default function SideNav() {
   return (
@@ -20,10 +19,7 @@ export default function SideNav() {
         <ThemeSwitch />
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form action={async () => {
-            'use server';
-            await signOut();
-          }}>
+        <form>
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,4 +1,4 @@
-'use client'
+// 'use client'
 import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
@@ -7,13 +7,13 @@ import {
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
-import { useFormState, useFormStatus } from 'react-dom';
-import { authenticate } from '@/app/lib/actions';
+// import { useFormState, useFormStatus } from 'react-dom';
+// import { authenticate } from '@/app/lib/actions';
 
 export default function LoginForm() {
-  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+  // const [errorMessage, dispatch] = useFormState(authenticate, undefined);
   return (
-    <form action={dispatch} className="space-y-3">
+    <form className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
           Please log in to continue.
@@ -60,7 +60,7 @@ export default function LoginForm() {
           </div>
         </div>
         <LoginButton />
-        <div
+        {/* <div
           className="flex h-8 items-end space-x-1"
           aria-live="polite"
           aria-atomic="true"
@@ -71,7 +71,7 @@ export default function LoginForm() {
               <p className="text-sm text-red-500">{errorMessage}</p>
             </>
           )}
-        </div>
+        </div> */}
        
       </div>
     </form>
@@ -79,10 +79,11 @@ export default function LoginForm() {
 }
 
 function LoginButton() {
-  const { pending } = useFormStatus();
+  // const { pending } = useFormStatus();
   return (
-    <Button className="mt-4 w-full" aria-disabled={pending}>
-      Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-    </Button>
+    // <Button className="mt-4 w-full" aria-disabled={pending}>
+    //   Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+    // </Button>
+    <button> Login</button>
   );
 }
